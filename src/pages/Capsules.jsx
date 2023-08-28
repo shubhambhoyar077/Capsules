b7cc90250ba13b985b04f33e77eb681102f7c348
shubham <shubham.bhoyar077@gmail.com>
2023-08-28 10:11:24 +0530

    import capsuleSlice

--- src/pages/Capsules.jsx
+++ src/pages/Capsules.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCapsules } from '../redux/capsules/capsulesSlice';
+
 const Capsules = () => {
-  return <h1>Capsules</h1>;
+  const { capsules, isLoading, error } = useSelector((state) => state.capsules);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCapsules()).then(() => {
+      console.log(capsules);
+    });
+  }, []);
+  return (
+    <ul>
+      {capsules.map((capsule) => (
+        <li>{capsule.capsule_serial}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Capsules;
