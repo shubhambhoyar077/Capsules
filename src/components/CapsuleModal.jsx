0f4f780b9176950793272973f0888f06bdf59599
shubham <shubham.bhoyar077@gmail.com>
2023-08-30 20:07:44 +0530

    Add styling to popup

--- src/components/CapsuleModal.jsx
+++ src/components/CapsuleModal.jsx
@@ -2,27 +2,61 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 const CapsuleModal = ({ capsule, modalIsOpen, closeModal }) => {
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
   return (
     <Modal
       isOpen={modalIsOpen}
       // onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
-      style={customStyles}
       contentLabel="Example Modal"
+      className="relative w-full h-screen bg-transparent flex justify-center items-center"
     >
-      <h2>{capsule.capsule_serial}</h2>
-      <button onClick={closeModal}>close</button>
-      <div>I am a modal</div>
+      <div className="relative bg-black rounded-lg shadow text-white w-4/5 max-w-xl">
+        <div className="flex items-center justify-between p-5 border-b rounded-t">
+          <h3 className="text-xl font-medium">{capsule.capsule_serial}</h3>
+          <br />
+          <button
+            type="button"
+            className=" bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+            onClick={closeModal}
+          >
+            X
+          </button>
+        </div>
+
+        <div className="p-6 space-y-6">
+          <h3 className="text-xl font-medium ">{capsule.details}</h3>
+          <h3 className="text-xl font-medium ">Status: {capsule.status}</h3>
+          <h3 className="text-xl font-medium ">Landings: {capsule.landings}</h3>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Flight
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {capsule.missions.map((mission) => (
+                <tr
+                  key={mission.name}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {mission.name}
+                  </th>
+                  <td className="px-6 py-4">{mission.flight}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </Modal>
   );
 };
