744139a0bb9c7dda3726c8ca104a6ec8ed5cdff6
shubham <shubham.bhoyar077@gmail.com>
2023-08-30 17:43:09 +0530

    Fix pagination logic as per search

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ReactPaginate from 'react-paginate';
 import { paginationCapsules } from '../redux/capsules/capsulesSlice';
 
 const Pagination = () => {
   const { searchList } = useSelector((state) => state.capsules);
-  const [itemOffset, setItemOffset] = useState(0);
   const dispatch = useDispatch();
 
   const itemsPerPage = 6;
-  const endOffset = itemOffset + itemsPerPage;
 
   useEffect(() => {
-    dispatch(
-      paginationCapsules({ itemOffset: itemOffset, endOffset: endOffset })
-    );
-    console.log(itemOffset);
-    console.log(endOffset);
-  }, [itemOffset, searchList]);
+    dispatch(paginationCapsules({ itemOffset: 0, endOffset: itemsPerPage }));
+  }, [searchList]);
 
   const pageCount = Math.ceil(searchList.length / itemsPerPage);
 
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % searchList.length;
-    setItemOffset(newOffset);
+
+    dispatch(
+      paginationCapsules({
+        itemOffset: newOffset,
+        endOffset: newOffset + itemsPerPage,
+      })
+    );
   };
 
   return (
